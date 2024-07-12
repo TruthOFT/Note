ba65a7f4cffafcd35aaf13e986c72d6cef8a2d80
Truth <2488292668@qq.com>
2024-07-12 10:10:31 +0800

    Mapper扫描

--- NoteBackend/src/main/java/com/note/NoteBackendApplication.java
+++ NoteBackend/src/main/java/com/note/NoteBackendApplication.java
@@ -1,9 +1,11 @@
 package com.note;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.note.mapper")
 public class NoteBackendApplication {
 
     public static void main(String[] args) {
