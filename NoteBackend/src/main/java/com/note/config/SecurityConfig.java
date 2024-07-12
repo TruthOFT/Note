eb71ddc5158d1d9cc11b48b7cb7db729fd90e357
Truth <2488292668@qq.com>
2024-07-12 11:07:56 +0800

    登录成功后用户信息更改

--- NoteBackend/src/main/java/com/note/config/SecurityConfig.java
+++ NoteBackend/src/main/java/com/note/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.note.config;
 
 import com.note.entity.RestBean;
+import com.note.entity.dto.Account;
 import com.note.entity.vo.response.AuthorizeVO;
 import com.note.filter.JwtFilter;
+import com.note.service.UserService;
 import com.note.utils.JwtUtil;
 import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
@@ -18,6 +20,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -32,13 +35,17 @@ public class SecurityConfig {
     @Resource
     JwtFilter jwtFilter;
 
+    @Resource
+    UserService userService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(login -> login.loginProcessingUrl("/api/auth/login")
-                        .successHandler(this::onAuthenticationSuccess))
+                        .successHandler(this::onAuthenticationSuccess)
+                        .failureHandler(this::onAuthenticationFailure))
                 .logout(logout -> logout.logoutUrl("/api/auth/logout")
                         .logoutSuccessHandler(this::onLogoutSuccess))
                 .exceptionHandling(conf -> conf.authenticationEntryPoint(this::commence))
@@ -51,9 +58,15 @@ public class SecurityConfig {
     private void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setContentType("application/json;charset=utf-8");
         User user = (User) authentication.getPrincipal();
-        String token = jwtUtil.createJwt(user, 1, user.getUsername());
-        AuthorizeVO authorizeVO = new AuthorizeVO(user.getUsername(), "", token, jwtUtil.expireTime());
+        Account account = userService.findUserByUsernameOrEmail(user.getUsername());
+        String token = jwtUtil.createJwt(user, account.getId(), account.getUsername());
+        AuthorizeVO authorizeVO = new AuthorizeVO(account.getUsername(), account.getRole(), token, jwtUtil.expireTime());
         response.getWriter().write(RestBean.success(authorizeVO, "登录成功").asJsonString());
+    }
+
+    private void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(RestBean.failure(401, null, exception.getMessage()).asJsonString());
     }
 
     private void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
